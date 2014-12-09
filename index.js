bec74133dc1b5f9506c7f1bf6ed105c9b1cc628c
Eunsub Kim <eunsub@gmail.com>
2014-12-09 14:38:54 +0900

    change C, P to downcase, add map with compose, pipe

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ var R = require('ramda');
 
 
 
-R.C = R.compose;
-R.P = R.pipe;
+R.c = R.compose;
+R.p = R.pipe;
+
+R.map.c = R.curry(function(fns, lst){
+  fns = [].concat(fns);
+  return R.map(R.apply(R.c, fns), lst);
+});
+R.map.p = R.curry(function(fns, lst){
+  fns = [].concat(fns);
+  return R.map(R.apply(R.p, fns), lst);
+});
 
 module.exports = R;
