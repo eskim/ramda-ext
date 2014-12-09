7480192ba6f1616303823370d66db7d244b52351
Eunsub Kim <eunsub@gmail.com>
2014-12-09 14:45:14 +0900

    add mapProp

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var R = require('ramda');
 R.c = R.compose;
 R.p = R.pipe;
 
+
+R.mapProp = R.curry(function(prop, lst){
+ return R.map(R.prop(prop), lst); 
+});
+
 R.map.c = R.curry(function(fns, lst){
   fns = [].concat(fns);
   return R.map(R.apply(R.c, fns), lst);
